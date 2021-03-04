2f4d54ae612b78a7874c71f96a6686daabda302a
lucas <laguero90@gmail.com>
2021-03-04 10:19:32 -0600

    connection to DB ready

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express ();
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require ('mongoose')
 
 
 // Midleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'))
 
 
 
@@ -12,6 +15,7 @@ require('dotenv/config');
 
 const api = process.env.API
 const backUrl = process.env.BACKEND_URL
+
 
 //          PORT, URL   / API  /  API END
 // http://localhost:3005/api/v1/products
@@ -21,6 +25,20 @@ app.get(`${api}/products`,(req,res) =>{
     res.send(' asd')
 })
 
+
+//Mongoose
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database',
+})
+.then(()=>{
+    console.log('Database conection ready')
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 app.listen (3005, ()=>{
     
