331744bb2be3f6751e0904102af47639cfab14c8
lucas <laguero90@gmail.com>
2021-03-04 12:44:25 -0600

    setting first backend routes

--- api/app.js
+++ api/app.js
@@ -4,26 +4,28 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require ('mongoose')
 
+//Models
+const Product = require('./models/product') 
+
+// CONSTANTS
+require('dotenv/config');
+
+const api = process.env.API
+const backUrl = process.env.BACKEND_URL
+const productsRouter = require('./routers/product')
+
+
 
 // Midleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'))
 
+app.use(`${api}/products`, productsRouter)
 
-
-require('dotenv/config');
-
-const api = process.env.API
-const backUrl = process.env.BACKEND_URL
 
 
 //          PORT, URL   / API  /  API END
 // http://localhost:3005/api/v1/products
-
-
-app.get(`${api}/products`,(req,res) =>{
-    res.send(' asd')
-})
 
 
 //Mongoose
@@ -40,6 +42,8 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err)
 })
 
+
+//APP LISTEN
 app.listen (3005, ()=>{
     
     console.log (`listening on ${backUrl}`)
