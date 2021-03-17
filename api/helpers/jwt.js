b08e1b9d38719711d87dc0a252c8cf08ccfb48ab
lucas <laguero90@gmail.com>
2021-03-17 08:36:38 -0500

    paths out of jwt with regex

--- api/helpers/jwt.js
+++ api/helpers/jwt.js
@@ -10,7 +10,9 @@ function authJwt (){
         algorithms:['HS256'] 
     }).unless({
         path:[
-            {url:`${api}/products`, methods:['GET', 'OPTIONS']},
+            //test on www.regex101.com with /\/api\/v1\/products(.*)/  and example: /api/v1/products/get/features/4
+            {url: /\/api\/v1\/products(.*)/, methods:['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods:['GET', 'OPTIONS']},
             `${api}/users/login`,
             `${api}/users/register`,
         ]
